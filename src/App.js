12b453cb766ef66e1047ad9427e45dd5d319f981
AchieversIT <achieversitsol@gmail.com>
2021-12-02 10:17:03 +0530

    components

---
+++ src/App.js
@@ -0,0 +1,6 @@
+const name = "React Js";
+const App = () => {
+  return <h1>{name}</h1>;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Component
+// There are two type Component
+// 1) class based Component
+// 2) function component
+
+ReactDOM.render(<App />, document.getElementById("root"));
